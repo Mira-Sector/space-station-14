7e3913f7aaade2abcd985f6e012ecb22178da74a
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-08-21 07:42:55 +0100

    horn and siren

--- Content.Shared/Vehicles/SharedVehicleComponent.cs
+++ Content.Shared/Vehicles/SharedVehicleComponent.cs
@@ -1,3 +1,4 @@
+using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 
@@ -9,8 +10,34 @@ public sealed partial class VehicleComponent : Component
     [ViewVariables]
     public EntityUid? Driver;
 
+    [ViewVariables]
+    public EntityUid? HornAction;
+
+    [ViewVariables]
+    public EntityUid? SirenAction;
+
+    public bool SirenEnabled = false;
+
+    public EntityUid? SirenStream;
+
+    /// <summary>
+    /// If non-zero how many virtual items to spawn on the driver
+    /// unbuckles them if they dont have enough
+    /// </summary>
     [DataField]
     public int RequiredHands = 1;
+
+    /// <summary>
+    /// What sound to play when the driver presses the horn action (plays once)
+    /// </summary>
+    [DataField]
+    public SoundSpecifier? HornSound = new SoundCollectionSpecifier("BikeHorn");
+
+    /// <summary>
+    /// What sound to play when the driver presses the siren action (loops)
+    /// </summary>
+    [DataField]
+    public SoundSpecifier? SirenSound;
 }
 [Serializable, NetSerializable]
 public enum VehicleState
--- Content.Shared/Vehicles/SharedVehicleSystem.cs
+++ Content.Shared/Vehicles/SharedVehicleSystem.cs
@@ -1,18 +1,27 @@
+using Content.Shared.Actions;
 using Content.Shared.Buckle;
 using Content.Shared.Buckle.Components;
 using Content.Shared.Hands;
 using Content.Shared.Inventory.VirtualItem;
 using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Systems;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Vehicles;
 
 public abstract partial class SharedVehicleSystem : EntitySystem
 {
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly SharedBuckleSystem _buckle = default!;
     [Dependency] private readonly SharedMoverController _mover = default!;
     [Dependency] private readonly SharedVirtualItemSystem _virtualItem = default!;
+
+    public static readonly EntProtoId HornActionId = "ActionHorn";
+    public static readonly EntProtoId SirenActionId = "ActionSiren";
 
     public override void Initialize()
     {
@@ -23,6 +32,9 @@ public abstract partial class SharedVehicleSystem : EntitySystem
         SubscribeLocalEvent<VehicleComponent, StrappedEvent>(OnStrapped);
         SubscribeLocalEvent<VehicleComponent, UnstrappedEvent>(OnUnstrapped);
         SubscribeLocalEvent<VehicleComponent, VirtualItemDeletedEvent>(OnDropped);
+
+        SubscribeLocalEvent<VehicleComponent, HornActionEvent>(OnHorn);
+        SubscribeLocalEvent<VehicleComponent, SirenActionEvent>(OnSiren);
     }
 
     private void OnInit(EntityUid uid, VehicleComponent component, ComponentInit args)
@@ -38,6 +50,35 @@ public abstract partial class SharedVehicleSystem : EntitySystem
         _buckle.Unbuckle(component.Driver.Value, component.Driver.Value);
         Dismount(component.Driver.Value, uid);
     }
+
+    private void OnHorn(EntityUid uid, VehicleComponent component, InstantActionEvent args)
+    {
+        if (component.Driver != args.Performer)
+            return;
+
+        _audio.PlayPvs(component.HornSound, component.Owner);
+    }
+
+    private void OnSiren(EntityUid uid, VehicleComponent component, InstantActionEvent args)
+    {
+        if (component.Driver != args.Performer)
+            return;
+
+        if (component.SirenSound == null)
+            return;
+
+        if (component.SirenEnabled)
+        {
+            component.SirenStream = _audio.Stop(component.SirenStream);
+        }
+        else
+        {
+            component.SirenStream = _audio.PlayPvs(component.SirenSound, component.Owner, AudioParams.Default.WithLoop(true).WithMaxDistance(5)).Value.Entity;
+        }
+
+        component.SirenEnabled = !component.SirenEnabled;
+    }
+
 
     private void OnStrapAttempt(Entity<VehicleComponent> ent, ref StrapAttemptEvent args)
     {
@@ -58,6 +99,12 @@ public abstract partial class SharedVehicleSystem : EntitySystem
                 return;
             }
         }
+
+        if (ent.Comp.HornSound != null)
+            _actions.AddAction(driver, ref ent.Comp.HornAction, HornActionId, ent);
+
+        if (ent.Comp.SirenSound != null)
+            _actions.AddAction(driver, ref ent.Comp.SirenAction, SirenActionId, ent);
     }
 
     private void OnStrapped(Entity<VehicleComponent> ent, ref StrappedEvent args)
@@ -108,8 +155,18 @@ public abstract partial class SharedVehicleSystem : EntitySystem
         _appearance.SetData(vehicle, VehicleState.Animated, false);
         vehicleComp.Driver = null;
 
+        if (vehicleComp.HornAction != null)
+            _actions.RemoveAction(driver, vehicleComp.HornAction);
+
+        if (vehicleComp.SirenAction != null)
+            _actions.RemoveAction(driver, vehicleComp.SirenAction);
+
         _virtualItem.DeleteInHandsMatching(driver, vehicle);
 
         return true;
     }
 }
+
+public sealed partial class HornActionEvent : InstantActionEvent;
+
+public sealed partial class SirenActionEvent : InstantActionEvent;
