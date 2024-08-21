d96854623e6bf1aa28559ce9fbbcdb45303525ca
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-08-21 14:22:12 +0100

    make north render behind

--- Content.Client/Vehicles/VehicleSystem.cs
+++ Content.Client/Vehicles/VehicleSystem.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Content.Shared.Vehicles;
 using Robust.Client.GameObjects;
 
@@ -12,6 +13,7 @@ public sealed class VehicleSystem : SharedVehicleSystem
     {
         base.Initialize();
         SubscribeLocalEvent<VehicleComponent, AppearanceChangeEvent>(OnAppearanceChange);
+        SubscribeLocalEvent<VehicleComponent, MoveEvent>(OnMove);
     }
 
     private void OnAppearanceChange(EntityUid uid, VehicleComponent comp, ref AppearanceChangeEvent args)
@@ -22,11 +24,27 @@ public sealed class VehicleSystem : SharedVehicleSystem
         if (!_appearance.TryGetData<bool>(uid, VehicleState.Animated, out bool animated))
             return;
 
+        if (!TryComp<SpriteComponent>(uid, out var spriteComp))
+            return;
+
+        SpritePos(uid);
+        spriteComp.LayerSetAutoAnimated(0, animated);
+    }
+
+    private void OnMove(EntityUid uid, VehicleComponent component, ref MoveEvent args)
+    {
+        SpritePos(uid);
+    }
+
+    private void SpritePos(EntityUid uid)
+    {
+        if (!TryComp<SpriteComponent>(uid, out var spriteComp))
+            return;
+
         if (!_appearance.TryGetData<bool>(uid, VehicleState.DrawOver, out bool depth))
             return;
 
-        if (!TryComp<SpriteComponent>(uid, out var spriteComp))
-            return;
+        var isNorth = Transform(uid).LocalRotation.GetCardinalDir() == Direction.North;
 
         if (depth)
         {
@@ -36,6 +54,10 @@ public sealed class VehicleSystem : SharedVehicleSystem
         {
             spriteComp.DrawDepth = (int)Content.Shared.DrawDepth.DrawDepth.Objects;
         }
-        spriteComp.LayerSetAutoAnimated(0, animated);
+
+        if (isNorth && depth)
+        {
+            spriteComp.DrawDepth = (int)Content.Shared.DrawDepth.DrawDepth.Objects;
+        }
     }
 }
