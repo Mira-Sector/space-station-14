8a37a05cb65923a645b9ac2e82311e7503a55e75
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-09-12 10:24:18 +0100

    alternate between footsteps

--- Content.Server/Footprints/Components/LeavesFootprintsComponent.cs
+++ Content.Server/Footprints/Components/LeavesFootprintsComponent.cs
@@ -7,17 +7,23 @@ public sealed partial class LeavesFootprintsComponent : Component
     /// How many footsteps to leave behind the player once they step on something which gives it
     /// </summary>
     [DataField]
-    public uint MaxFootsteps = 3;
+    public uint MaxFootsteps = 8;
 
     /// <summary>
     /// How far should the player have to walk until we leave a footprint
     /// </summary>
     [DataField]
-    public float Distance = 1.5f;
+    public float Distance = 0.8f;
 
     /// <summary>
     /// What decal to leave behind when the entity moves.
     /// </summary>
     [DataField]
-    public string FootprintDecal = "Box";
+    public string FootprintDecal = "FootLeft";
+
+    /// <summary>
+    /// If set with will alternate between this decal and the regular decal
+    /// </summary>
+    [DataField]
+    public string FootprintDecalAlternative = "FootRight";
 }
--- Content.Server/Footprints/Systems/UpdateFootprintsSystem.cs
+++ Content.Server/Footprints/Systems/UpdateFootprintsSystem.cs
@@ -1,6 +1,7 @@
 using Content.Server.Footprints.Components;
 using Content.Shared.Fluids;
 using Robust.Shared.Physics.Events;
+using Robust.Shared.Random;
 
 namespace Content.Server.Footprint.Systems;
 
@@ -8,11 +9,25 @@ public sealed class UpdateFootprintystem : EntitySystem
 {
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
 
     /// <inheritdoc/>
     public override void Initialize()
     {
         SubscribeLocalEvent<GivesFootprintsComponent, EndCollideEvent>(OnStep);
+        SubscribeLocalEvent<CanLeaveFootprintsComponent, ComponentInit>(OnInit);
+    }
+
+    private void OnInit(EntityUid uid, CanLeaveFootprintsComponent component, ComponentInit args)
+    {
+        if (!TryComp<LeavesFootprintsComponent>(uid, out var footprintComp))
+        {
+            RemComp<CanLeaveFootprintsComponent>(uid);
+            return;
+        }
+
+        if (footprintComp.FootprintDecalAlternative != null)
+            component.UseAlternative = _random.Prob(0.5f);
     }
 
     private void OnStep(EntityUid uid, GivesFootprintsComponent component, ref EndCollideEvent args)
@@ -25,7 +40,9 @@ public sealed class UpdateFootprintystem : EntitySystem
         var color = playerFootprintComp.Color;
 
         if (_appearance.TryGetData<Color>(uid, PuddleVisuals.SolutionColor, out color))
+        {
             color *= playerFootprintComp.Color;
+        }
 
         playerFootprintComp.LastFootstep = _transform.GetMapCoordinates(args.OtherEntity);
         playerFootprintComp.FootstepsLeft = footprintComp.MaxFootsteps;
