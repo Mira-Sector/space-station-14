2f99e96d6e81e93a6247c77cde2ec52fa424b332
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-09-12 13:05:39 +0100

    make shoes have prints

--- Content.Server/Footprints/Systems/LeaveFootprintSystem.cs
+++ Content.Server/Footprints/Systems/LeaveFootprintSystem.cs
@@ -1,6 +1,9 @@
 using Content.Server.Decals;
 using Content.Server.Footprints.Components;
+using Content.Shared.Clothing.Components;
 using Content.Shared.Decals;
+using Content.Shared.Inventory;
+using Robust.Shared.Containers;
 using Robust.Shared.Map;
 using Robust.Shared.Maths;
 using Robust.Shared.Prototypes;
@@ -9,10 +12,12 @@ using System.Numerics;
 namespace Content.Server.Footprint.Systems;
 
 
-public sealed class LeaveFootprintystem : EntitySystem
+public sealed partial class FootprintSystem : EntitySystem
 {
 
+    [Dependency] private readonly SharedContainerSystem _container = default!;
     [Dependency] private readonly DecalSystem _decals = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly SharedMapSystem _map = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
@@ -23,51 +28,58 @@ public sealed class LeaveFootprintystem : EntitySystem
 
     const float DeltaAlpha = MaxAlpha - MinAlpha;
 
+    const string ShoeSlot = "shoes";
+
     public override void Update(float frametime)
     {
         var query = EntityQueryEnumerator<CanLeaveFootprintsComponent, TransformComponent>();
 
         while (query.MoveNext(out var uid, out var currentFootprintComp, out var transform))
         {
-            if (!EntityManager.TryGetComponent<LeavesFootprintsComponent>(uid, out var playerFootprintComp))
-            {
-                RemComp<CanLeaveFootprintsComponent>(uid);
+            if (!CanLeaveFootprints(uid, out var messMaker) ||
+                !TryComp<LeavesFootprintsComponent>(messMaker, out var footprintComp))
                 continue;
+
+            var posUid = messMaker;
+            var angle = transform.LocalRotation;
+
+            if (HasComp<ClothingComponent>(messMaker) &&
+                _container.TryGetContainingContainer((messMaker, null, null), out var container) &&
+                TryComp<TransformComponent>(container.Owner, out var xform))
+            {
+                posUid = container.Owner;
+                angle = xform.LocalRotation;
             }
 
-            var newPos = _transform.GetMapCoordinates(uid);
+            var newPos = _transform.GetMapCoordinates(posUid);
             var oldPos = currentFootprintComp.LastFootstep;
 
             if (newPos == MapCoordinates.Nullspace)
                 continue;
 
-            var worldPos = _transform.GetWorldPosition(uid);
-
-            var angle = transform.LocalRotation;
-
             if (newPos.MapId != oldPos.MapId)
             {
-                DoFootprint(uid, currentFootprintComp, playerFootprintComp, newPos, angle);
+                DoFootprint(messMaker, currentFootprintComp, footprintComp, newPos, angle);
                 return;
             }
 
             var delta = Vector2.Distance(newPos.Position, oldPos.Position);
 
-            if (delta < playerFootprintComp.Distance)
+            if (delta < footprintComp.Distance)
                 continue;
 
-            DoFootprint(uid, currentFootprintComp, playerFootprintComp, newPos, angle);
+            DoFootprint(messMaker, currentFootprintComp, footprintComp, newPos, angle);
         }
     }
 
-    private void DoFootprint(EntityUid uid, CanLeaveFootprintsComponent currentFootprintComp, LeavesFootprintsComponent playerFootprintComp, MapCoordinates pos, Angle angle)
+    private void DoFootprint(EntityUid uid, CanLeaveFootprintsComponent currentFootprintComp, LeavesFootprintsComponent footprintComp, MapCoordinates pos, Angle angle)
     {
-        var decal = playerFootprintComp.FootprintDecal;
+        var decal = footprintComp.FootprintDecal;
 
         if (currentFootprintComp.UseAlternative != null)
         {
             if (currentFootprintComp.UseAlternative.Value)
-                decal = playerFootprintComp.FootprintDecalAlternative;
+                decal = footprintComp.FootprintDecalAlternative;
 
             currentFootprintComp.UseAlternative ^= true;
         }
@@ -83,7 +95,7 @@ public sealed class LeaveFootprintystem : EntitySystem
 
         var color = currentFootprintComp.Color;
 
-        float onePiece = DeltaAlpha / (playerFootprintComp.MaxFootsteps); //IS REAL!!
+        float onePiece = DeltaAlpha / (footprintComp.MaxFootsteps); //IS REAL!!
         color = color.WithAlpha(MinAlpha + (onePiece * currentFootprintComp.FootstepsLeft));
 
         var coords = new EntityCoordinates(gridUid, _map.WorldToLocal(gridUid, grid, pos.Position));
@@ -100,5 +112,45 @@ public sealed class LeaveFootprintystem : EntitySystem
 
         currentFootprintComp.LastFootstep = pos;
         currentFootprintComp.Color = color;
+    }
+
+    private bool CanLeaveFootprints(EntityUid uid, out EntityUid messMaker)
+    {
+        messMaker = EntityUid.Invalid;
+
+        if (_inventory.TryGetSlotEntity(uid, ShoeSlot, out var shoe) &&
+            EntityManager.HasComponent<LeavesFootprintsComponent>(shoe)) // check if their shoes have it too
+        {
+            messMaker = shoe.Value;
+        }
+        else if (EntityManager.HasComponent<LeavesFootprintsComponent>(uid))
+        {
+            if (shoe != null)
+                RemComp<CanLeaveFootprintsComponent>(shoe.Value);
+
+            messMaker = uid;
+        }
+        else
+        {
+            CleanupFootprintComp(uid, shoe);
+            return false;
+        }
+
+        if (messMaker == EntityUid.Invalid ||
+            !HasComp<LeavesFootprintsComponent>(messMaker))
+        {
+            CleanupFootprintComp(uid, shoe);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void CleanupFootprintComp(EntityUid player, EntityUid? shoe)
+    {
+        RemComp<CanLeaveFootprintsComponent>(player);
+
+        if (shoe != null)
+            RemComp<CanLeaveFootprintsComponent>(shoe.Value);
     }
 }
