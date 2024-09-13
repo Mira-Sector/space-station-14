ce20a4f9f836a26a9a49489c6c9e5530f6444e2c
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-09-13 20:21:46 +0100

    Revert "alternate between footsteps"
    This reverts commit 8a37a05cb65923a645b9ac2e82311e7503a55e75.

--- Content.Server/Footprints/Systems/LeaveFootprintSystem.cs
+++ Content.Server/Footprints/Systems/LeaveFootprintSystem.cs
@@ -8,20 +8,13 @@ using System.Numerics;
 
 namespace Content.Server.Footprint.Systems;
 
-
 public sealed class LeaveFootprintystem : EntitySystem
 {
-
     [Dependency] private readonly DecalSystem _decals = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly SharedMapSystem _map = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly IMapManager _mapManager = default!;
-
-    const float MaxAlpha = 0.5f;
-    const float MinAlpha = 0.2f;
-
-    const float DeltaAlpha = MaxAlpha - MinAlpha;
 
     public override void Update(float frametime)
     {
@@ -41,9 +34,7 @@ public sealed class LeaveFootprintystem : EntitySystem
             if (newPos == MapCoordinates.Nullspace)
                 continue;
 
-            var worldPos = _transform.GetWorldPosition(uid);
-
-            var angle = transform.LocalRotation;
+            var angle = Angle.FromWorldVec(newPos.Position);
 
             if (newPos.MapId != oldPos.MapId)
             {
@@ -62,17 +53,7 @@ public sealed class LeaveFootprintystem : EntitySystem
 
     private void DoFootprint(EntityUid uid, CanLeaveFootprintsComponent currentFootprintComp, LeavesFootprintsComponent playerFootprintComp, MapCoordinates pos, Angle angle)
     {
-        var decal = playerFootprintComp.FootprintDecal;
-
-        if (currentFootprintComp.UseAlternative != null)
-        {
-            if (currentFootprintComp.UseAlternative.Value)
-                decal = playerFootprintComp.FootprintDecalAlternative;
-
-            currentFootprintComp.UseAlternative ^= true;
-        }
-
-        if (!_prototypeManager.TryIndex<DecalPrototype>(decal, out var footprintDecal))
+        if (!_prototypeManager.TryIndex<DecalPrototype>(playerFootprintComp.FootprintDecal, out var footprintDecal))
         {
             RemComp<CanLeaveFootprintsComponent>(uid);
             return;
@@ -83,22 +64,13 @@ public sealed class LeaveFootprintystem : EntitySystem
 
         var color = currentFootprintComp.Color;
 
-        float onePiece = DeltaAlpha / (playerFootprintComp.MaxFootsteps); //IS REAL!!
-        color = color.WithAlpha(MinAlpha + (onePiece * currentFootprintComp.FootstepsLeft));
-
         var coords = new EntityCoordinates(gridUid, _map.WorldToLocal(gridUid, grid, pos.Position));
 
         _decals.TryAddDecal(footprintDecal.ID, coords, out _, color, angle, cleanable: true);
-
+        currentFootprintComp.LastFootstep = pos;
         currentFootprintComp.FootstepsLeft -= 1;
 
         if (currentFootprintComp.FootstepsLeft <= 0)
-        {
             RemComp<CanLeaveFootprintsComponent>(uid);
-            return;
-        }
-
-        currentFootprintComp.LastFootstep = pos;
-        currentFootprintComp.Color = color;
     }
 }
--- Content.Server/Footprints/Systems/UpdateFootprintsSystem.cs
+++ Content.Server/Footprints/Systems/UpdateFootprintsSystem.cs
@@ -1,7 +1,6 @@
 using Content.Server.Footprints.Components;
 using Content.Shared.Fluids;
 using Robust.Shared.Physics.Events;
-using Robust.Shared.Random;
 
 namespace Content.Server.Footprint.Systems;
 
@@ -9,25 +8,11 @@ public sealed class UpdateFootprintystem : EntitySystem
 {
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-    [Dependency] private readonly IRobustRandom _random = default!;
 
     /// <inheritdoc/>
     public override void Initialize()
     {
         SubscribeLocalEvent<GivesFootprintsComponent, EndCollideEvent>(OnStep);
-        SubscribeLocalEvent<CanLeaveFootprintsComponent, ComponentInit>(OnInit);
-    }
-
-    private void OnInit(EntityUid uid, CanLeaveFootprintsComponent component, ComponentInit args)
-    {
-        if (!TryComp<LeavesFootprintsComponent>(uid, out var footprintComp))
-        {
-            RemComp<CanLeaveFootprintsComponent>(uid);
-            return;
-        }
-
-        if (footprintComp.FootprintDecalAlternative != null)
-            component.UseAlternative = _random.Prob(0.5f);
     }
 
     private void OnStep(EntityUid uid, GivesFootprintsComponent component, ref EndCollideEvent args)
@@ -40,9 +25,7 @@ public sealed class UpdateFootprintystem : EntitySystem
         var color = playerFootprintComp.Color;
 
         if (_appearance.TryGetData<Color>(uid, PuddleVisuals.SolutionColor, out color))
-        {
             color *= playerFootprintComp.Color;
-        }
 
         playerFootprintComp.LastFootstep = _transform.GetMapCoordinates(args.OtherEntity);
         playerFootprintComp.FootstepsLeft = footprintComp.MaxFootsteps;
