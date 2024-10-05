227042f6757726d0ac2a2371f177901fbc726284
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-10-05 23:51:35 +0100

    Reapply "Vent crawling (#60)" (#77)
    This reverts commit 576fcb3b0e25a6f2ad443ab797d9847906bec28d.

---
+++ Content.Shared/Atmos/Piping/Crawling/Components/CanEnterPipeCrawlingComponent.cs
@@ -0,0 +1,8 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Atmos.Piping.Crawling.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CanEnterPipeCrawlingComponent : Component
+{
+}
