d9751cd583317ab39cde86858af2f363f8204ea6
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-10-12 02:07:03 +0100

    pls work

--- Content.Shared/Inventory/InventoryComponent.cs
+++ Content.Shared/Inventory/InventoryComponent.cs
@@ -1,4 +1,4 @@
-using Content.Shared.DisplacementMap;
+﻿using Content.Shared.DisplacementMap;
 using Robust.Shared.Containers;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
@@ -14,10 +14,7 @@ public sealed partial class InventoryComponent : Component
 
     [DataField("speciesId")] public string? SpeciesId { get; set; }
 
-    [ViewVariables]
     public SlotDefinition[] Slots = Array.Empty<SlotDefinition>();
-
-    [ViewVariables]
     public ContainerSlot[] Containers = Array.Empty<ContainerSlot>();
 
     [DataField]
