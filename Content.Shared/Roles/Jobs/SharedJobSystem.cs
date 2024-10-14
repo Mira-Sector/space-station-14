ef1f10b7f47b93055d4dfee11993e028533892aa
Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
2024-10-14 16:05:25 +1300

    Add role prototype validation tests (#32801)
    * Add role prototype validation test
    
    * Rejig GetPrototypesWithComponent
    
    * More tests n stuff

--- Content.Shared/Roles/Jobs/SharedJobSystem.cs
+++ Content.Shared/Roles/Jobs/SharedJobSystem.cs
@@ -102,7 +102,6 @@ public abstract class SharedJobSystem : EntitySystem
 
     public bool MindHasJobWithId(EntityUid? mindId, string prototypeId)
     {
-        MindRoleComponent? comp = null;
         if (mindId is null)
             return false;
 
@@ -111,9 +110,7 @@ public abstract class SharedJobSystem : EntitySystem
         if (role is null)
             return false;
 
-        comp = role.Value;
-
-        return (comp.JobPrototype == prototypeId);
+        return role.Value.Comp1.JobPrototype == prototypeId;
     }
 
     public bool MindTryGetJob(
@@ -123,7 +120,7 @@ public abstract class SharedJobSystem : EntitySystem
         prototype = null;
         MindTryGetJobId(mindId, out var protoId);
 
-        return (_prototypes.TryIndex<JobPrototype>(protoId, out prototype) || prototype is not null);
+        return _prototypes.TryIndex(protoId, out prototype) || prototype is not null;
     }
 
     public bool MindTryGetJobId(
@@ -136,9 +133,9 @@ public abstract class SharedJobSystem : EntitySystem
             return false;
 
         if (_roles.MindHasRole<JobRoleComponent>(mindId.Value, out var role))
-            job = role.Value.Comp.JobPrototype;
+            job = role.Value.Comp1.JobPrototype;
 
-        return (job is not null);
+        return job is not null;
     }
 
     /// <summary>
