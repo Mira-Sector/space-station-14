ebddaccbeae13c573081dc36425ad51f103d8a5d
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-10-22 00:41:08 +0100

    fix godmode

--- Content.Server/Administration/Systems/RejuvenateSystem.cs
+++ Content.Server/Administration/Systems/RejuvenateSystem.cs
@@ -1,11 +1,27 @@
-﻿using Content.Shared.Rejuvenate;
+using Content.Shared.Body.Components;
+using Content.Shared.Body.Systems;
+using Content.Shared.Rejuvenate;
 
 namespace Content.Server.Administration.Systems;
 
 public sealed class RejuvenateSystem : EntitySystem
 {
+    [Dependency] private readonly SharedBodySystem _body = default!;
+
     public void PerformRejuvenate(EntityUid target)
     {
-        RaiseLocalEvent(target, new RejuvenateEvent());
+        if (TryComp<BodyComponent>(target, out var bodyComp))
+        {
+            var parts = _body.GetBodyChildren(target, bodyComp);
+
+            foreach ((var currentPart, var _) in parts)
+            {
+                RaiseLocalEvent(currentPart, new RejuvenateEvent());
+            }
+        }
+        else
+        {
+            RaiseLocalEvent(target, new RejuvenateEvent());
+        }
     }
 }
