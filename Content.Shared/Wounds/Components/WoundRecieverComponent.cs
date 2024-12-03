ebe15f858706b30b128abb5f792de387aa460238
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2024-12-03 02:13:25 +0000

    merge woundcomp into woundrecievercomp

--- Content.Shared/Wounds/Components/WoundRecieverComponent.cs
+++ Content.Shared/Wounds/Components/WoundRecieverComponent.cs
@@ -1,4 +1,7 @@
+using Content.Shared.Surgery.Prototypes;
+using Content.Shared.Wounds.Prototypes;
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Shared.Wounds.Components;
 
@@ -7,4 +10,31 @@ public sealed partial class WoundRecieverComponent : Component
 {
     [DataField]
     public List<string> SelectableWounds = new ();
+
+    [ViewVariables]
+    public WoundPrototype? CurrentWound;
+
+    [DataField(customTypeSerializer:typeof(PrototypeIdSerializer<SurgeryPrototype>))]
+    public string Graph { get; set; } = string.Empty;
+
+    [DataField]
+    public string Node { get; set; } = default!;
+
+    [DataField]
+    public int? EdgeIndex { get; set; } = null;
+
+    [DataField]
+    public int StepIndex { get; set; } = 0;
+
+    [DataField]
+    public string? TargetNode { get; set; } = null;
+
+    [ViewVariables]
+    public int? TargetEdgeIndex { get; set; } = null;
+
+    [ViewVariables]
+    public Queue<string>? NodePathfinding { get; set; } = null;
+
+    [ViewVariables]
+    public readonly Queue<(EntityUid, object)> InteractionQueue = new();
 }
