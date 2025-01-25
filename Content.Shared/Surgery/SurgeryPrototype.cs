6187f8b6fa394d46e2bec50cc2d5d774b56be3cd
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-01-25 03:55:52 +0000

    fix duplicate node ids and missing nodes

--- Content.Shared/Surgery/SurgeryPrototype.cs
+++ Content.Shared/Surgery/SurgeryPrototype.cs
@@ -1,5 +1,6 @@
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
+using System.IO;
 
 namespace Content.Shared.Surgery;
 
@@ -11,15 +12,24 @@ public sealed partial class SurgeryPrototype : SurgeryGraph, IPrototype, ISerial
 
     void ISerializationHooks.AfterDeserialization()
     {
+        HashSet<string> nodeIds = new();
         foreach (var node in Nodes)
         {
+            if (node.Id != null)
+            {
+                if (nodeIds.Contains(node.Id))
+                    throw new InvalidDataException($"Duplicate node ID {node.Id} in surgery graph {ID}");
+
+                nodeIds.Add(node.Id);
+            }
+
             foreach (var edge in node.Edges)
             {
                 if (edge._connection is not {})
                     continue;
 
                 if (!TryFindNode(edge._connection, out var connection))
-                    continue;
+                    throw new InvalidDataException($"Cannot find node {edge._connection} in surgery graph {ID}");
 
                 edge.Connection = connection;
                 connection.Connections.Add(edge);
