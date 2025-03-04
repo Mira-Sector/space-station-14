ad9b89d801400f9096b2306a9f51bc780b9fc455
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-03-04 01:06:07 +0000

    fix: misc malf nuke module issues
    * reset the nuke timer on every nuke module
    
    * keep track of the active ai nukes
    
    * timespan cleanup
    
    * use the remaining time with more delay
    
    * disarm the nukes when intelicarded
    
    * properly cleanup on disarm
    
    * round now ends when the nuke goes off

--- Content.Server/Silicons/StationAi/Modules/NukeModule.cs
+++ Content.Server/Silicons/StationAi/Modules/NukeModule.cs
@@ -1,4 +1,5 @@
 using Content.Server.Nuke;
+using Content.Server.RoundEnd;
 using Content.Shared.Silicons.StationAi;
 using Content.Shared.Silicons.StationAi.Modules;
 
@@ -7,12 +8,17 @@ namespace Content.Server.Silicons.StationAi.Modules;
 public sealed class NukeModuleSystem : EntitySystem
 {
     [Dependency] private readonly NukeSystem _nuke = default!;
+    [Dependency] private readonly RoundEndSystem _roundEnd = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<StationAiCanHackComponent, StationAiNukeEvent>(OnAction);
+        SubscribeLocalEvent<NukeModuleStationAiComponent, GotIntellicardedEvent>(OnIntellicarded);
+
+        SubscribeLocalEvent<NukeModuleNukeComponent, NukeDisarmSuccessEvent>(RemoveNuke);
+        SubscribeLocalEvent<NukeModuleNukeComponent, NukeExplodedEvent>(OnNukeExploded);
     }
 
     private void OnAction(EntityUid uid, StationAiCanHackComponent component, StationAiNukeEvent args)
@@ -30,8 +36,19 @@ public sealed class NukeModuleSystem : EntitySystem
             if (aiXForm.MapUid != nukeXForm.MapUid)
                 continue;
 
-            _nuke.ArmBomb(nukeUid, nukeComp);
-            args.Handled = true;
+            var prevTime = nukeComp.RemainingTime;
+            _nuke.SetRemainingTime(nukeUid, Math.Min(nukeComp.Timer, nukeComp.RemainingTime + args.AdditionalDelay), nukeComp);
+
+            if (!_nuke.ArmBomb(nukeUid, nukeComp))
+            {
+                _nuke.SetRemainingTime(nukeUid, prevTime, nukeComp);
+                continue;
+            }
+
+            EnsureComp<NukeModuleNukeComponent>(nukeUid).StationAi = args.Performer;
+            EnsureComp<NukeModuleStationAiComponent>(args.Performer).Nukes.Add(nukeUid);
+
+            args.Handled |= true;
         }
 
         // dont let them waste their 130 power
@@ -43,5 +60,34 @@ public sealed class NukeModuleSystem : EntitySystem
 
         EntityManager.DeleteEntity(args.Action);
     }
-}
 
+    private void RemoveNuke(EntityUid uid, NukeModuleNukeComponent component, NukeDisarmSuccessEvent args)
+    {
+        if (TryComp<NukeModuleStationAiComponent>(component.StationAi, out var aiComp))
+            aiComp.Nukes.Remove(uid);
+
+        RemCompDeferred(uid, component);
+    }
+
+    private void OnIntellicarded(EntityUid uid, NukeModuleStationAiComponent component, GotIntellicardedEvent args)
+    {
+        foreach (var nuke in component.Nukes)
+        {
+            _nuke.DisarmBomb(nuke);
+            RemComp<NukeModuleNukeComponent>(nuke);
+        }
+
+        RemCompDeferred(uid, component);
+    }
+
+    private void OnNukeExploded(EntityUid uid, NukeModuleNukeComponent component, NukeExplodedEvent ev)
+    {
+        if (ev.OwningStation is not {} nukeStation)
+            return;
+
+        if (Transform(component.StationAi).GridUid != nukeStation)
+            return;
+
+        _roundEnd.EndRound();
+    }
+}
--- Content.Shared/Silicons/StationAi/Modules/SharedNukeModule.cs
+++ Content.Shared/Silicons/StationAi/Modules/SharedNukeModule.cs
@@ -2,4 +2,13 @@ using Content.Shared.Actions;
 
 namespace Content.Shared.Silicons.StationAi.Modules;
 
-public sealed partial class StationAiNukeEvent : InstantActionEvent;
+public sealed partial class StationAiNukeEvent : InstantActionEvent
+{
+    [DataField]
+    public float AdditionalDelay;
+
+    public StationAiNukeEvent(float delay)
+    {
+        AdditionalDelay = delay;
+    }
+}
