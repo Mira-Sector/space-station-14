1a927c1c9d36b7d641975ff6a16a1d8f48e4a5db
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-03-12 13:37:42 +0000

    tray can toggle which layers to show

--- Content.Client/Atmos/EntitySystems/PipeLayerableSystem.cs
+++ Content.Client/Atmos/EntitySystems/PipeLayerableSystem.cs
@@ -1,5 +1,6 @@
-using Content.Client.Items;
 using Content.Client.Atmos.UI;
+using Content.Client.Items;
+using Content.Client.SubFloor;
 using Content.Shared.Atmos.Piping.Layerable;
 
 namespace Content.Client.Atmos.EntitySystems;
@@ -10,5 +11,13 @@ public sealed partial class PipeLayerableSystem : EntitySystem
     {
         base.Initialize();
         Subs.ItemStatus<PipeLayerableComponent>(entity => new PipeLayerableControl(entity.Comp));
+
+        SubscribeLocalEvent<PipeLayerableComponent, TrayCanRevealEvent>(OnCanReveal);
+    }
+
+    private void OnCanReveal(Entity<PipeLayerableComponent> ent, ref TrayCanRevealEvent args)
+    {
+        if (!args.Tray.Comp.RevealedLayers.Contains(ent.Comp.Layer))
+            args.Cancel();
     }
 }
