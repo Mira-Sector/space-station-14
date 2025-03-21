abe09f701e7388c6f8cfd83e09cef63fbaa1f6e1
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-03-21 05:06:50 +0000

    database migrations

---
+++ Content.Server.Database/Migrations/Sqlite/20250321044057_Mimic.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class Mimic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "mimic_phrase_prob",
+                columns: table => new
+                {
+                    mimic_phrase_prob_id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    prototype_id = table.Column<string>(type: "TEXT", nullable: false),
+                    phrase = table.Column<string>(type: "TEXT", nullable: false),
+                    prob = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_mimic_phrase_prob", x => x.mimic_phrase_prob_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "mimic_phrase_prob");
+        }
+    }
+}
