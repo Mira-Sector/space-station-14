2b6c45cde3753c184830f43848f7b3755d5ff752
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-04-20 07:36:35 +0100

    manually draw sprites

--- Content.Client/XRay/XRayOverlay.cs
+++ Content.Client/XRay/XRayOverlay.cs
@@ -13,6 +13,7 @@ public sealed class XRayOverlay : Overlay
     [Dependency] private readonly IEntityManager _entityManager = default!;
     [Dependency] private readonly IPrototypeManager _prototype = default!;
 
+    private readonly SpriteSystem _sprite;
     private readonly SharedTransformSystem _transform;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
@@ -23,10 +24,8 @@ public sealed class XRayOverlay : Overlay
     public XRayOverlay()
     {
         IoCManager.InjectDependencies(this);
+        _sprite = _entityManager.System<SpriteSystem>();
         _transform = _entityManager.System<SharedTransformSystem>();
-
-        // needs to be drawn after the meson goggles screen overlay
-        ZIndex = 1;
     }
 
     protected override bool BeforeDraw(in OverlayDrawArgs args)
@@ -36,30 +35,48 @@ public sealed class XRayOverlay : Overlay
 
     protected override void Draw(in OverlayDrawArgs args)
     {
-        if (ScreenTexture == null)
-            return;
-
-        var worldHandle = args.WorldHandle;
-
         foreach (var (xray, entities) in Entities)
         {
             var shader = _prototype.Index<ShaderPrototype>(xray.Comp.Shader).InstanceUnique();
+            args.WorldHandle.UseShader(shader);
 
             foreach (var entity in entities)
             {
                 if (!_entityManager.TryGetComponent<SpriteComponent>(entity, out var sprite))
                     continue;
 
-                sprite.PostShader = shader;
+                var entityXform = _entityManager.GetComponent<TransformComponent>(entity);
+                var (entPos, entRot, worldMatrix) = _transform.GetWorldPositionRotationMatrix(entityXform);
 
-                var (entPos, entRot) = _transform.GetWorldPositionRotation(entity);
+                var eyeRot = args.Viewport.Eye?.Rotation ?? default;
+                var spriteRot = eyeRot - entRot;
 
-                var rot = args.Viewport.Eye?.Rotation ?? default;
-                sprite.Render(worldHandle, rot, entRot, null, entPos);
+                var entityMatrix = Matrix3Helpers.CreateTransform(entPos, spriteRot);
+                args.DrawingHandle.SetTransform(worldMatrix);
+
+                var layerRot = sprite.NoRotation || sprite.SnapCardinals ? eyeRot : spriteRot;
+
+                foreach (var spriteLayer in sprite.AllLayers)
+                {
+                    if (spriteLayer is not SpriteComponent.Layer layer || !_sprite.IsVisible(layer))
+                        continue;
+
+                    if (layer.ActualRsi is not { } rsi || !rsi.TryGetState(layer.State, out var rsiState))
+                        continue;
+
+                    var dir = SpriteComponent.Layer.GetDirection(rsiState.RsiDirections, spriteRot);
+                    var texture = rsiState.GetFrame(dir, 0);
+
+                    var textureSize = texture.Size / (float)EyeManager.PixelsPerMeter;
+                    var quad = Box2.FromDimensions(textureSize / -2, textureSize);
+                    var quadRotated = new Box2Rotated(quad, layerRot);
+
+                    args.WorldHandle.DrawTextureRect(texture, quadRotated);
+                }
             }
         }
 
-        worldHandle.UseShader(null);
-        worldHandle.SetTransform(Matrix3x2.Identity);
+        args.DrawingHandle.SetTransform(Matrix3x2.Identity);
+        args.WorldHandle.UseShader(null);
     }
 }
