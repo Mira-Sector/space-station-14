965236df72e106bc60ffcae73b7ad24368aec315
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-04-21 16:02:04 +0100

    show floor tiles

--- Content.Client/XRay/ShowXRaySystem.cs
+++ Content.Client/XRay/ShowXRaySystem.cs
@@ -4,18 +4,22 @@ using Content.Shared.Inventory.Events;
 using Content.Shared.Physics;
 using Content.Shared.XRay;
 using Content.Shared.Whitelist;
+using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Systems;
 using Robust.Shared.Timing;
 using System.Linq;
-using Robust.Client.GameObjects;
+using System.Numerics;
 
 namespace Content.Client.XRay;
 
 public sealed class ShowXRaySystem : EquipmentHudSystem<ShowXRayComponent>
 {
     [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly SharedMapSystem _map = default!;
     [Dependency] private readonly IOverlayManager _overlayMan = default!;
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
@@ -48,6 +52,7 @@ public sealed class ShowXRaySystem : EquipmentHudSystem<ShowXRayComponent>
             }
 
             _overlay.Entities.Remove((uid, component));
+            _overlay.Tiles.Remove((uid, component));
             UpdateEntities((uid, component));
         }
     }
@@ -65,6 +70,7 @@ public sealed class ShowXRaySystem : EquipmentHudSystem<ShowXRayComponent>
             _overlayMan.AddOverlay(_overlay);
 
         _overlay.Entities.Clear();
+        _overlay.Tiles.Clear();
 
         foreach (var component in args.Components)
         {
@@ -81,7 +87,7 @@ public sealed class ShowXRaySystem : EquipmentHudSystem<ShowXRayComponent>
 
     private void UpdateEntities(Entity<ShowXRayComponent> ent)
     {
-        _overlay.Entities.Add(ent, new HashSet<EntityUid>());
+        _overlay.Entities.Add(ent, new());
         var entities = _lookup.GetEntitiesInRange(ent.Owner, ent.Comp.Range);
 
         var xrayTransform = Transform(ent.Owner);
@@ -93,23 +99,50 @@ public sealed class ShowXRaySystem : EquipmentHudSystem<ShowXRayComponent>
             if (!_whitelist.CheckBoth(entity, ent.Comp.Blacklist, ent.Comp.Whitelist))
                 continue;
 
-            if (!TryComp<SpriteComponent>(entity, out var sprite))
+            if (!HasComp<SpriteComponent>(entity))
                 continue;
 
             var entityPos = _transform.GetWorldPosition(entity);
-            var delta = entityPos - xrayPos;
-            var distance = delta.Length();
-
-            var ray = new CollisionRay(xrayPos, delta.Normalized(), (int)CollisionGroup.SingularityLayer);
-            var rayCastResults = _physics.IntersectRayWithPredicate(xrayMapId, ray, distance, e => !HasComp<OccluderComponent>(e) || e == entity);
 
             // must be hidden
-            if (!rayCastResults.Any())
+            if (!EntitiesBlocking(xrayPos, xrayMapId, entityPos))
                 continue;
 
             _overlay.Entities[ent].Add(entity);
         }
 
+        _overlay.Tiles.Add(ent, new());
+
+        var radius = new Circle(xrayPos, ent.Comp.Range);
+        var grids = _lookup.GetEntitiesInRange<MapGridComponent>(xrayTransform.Coordinates, ent.Comp.Range);
+
+        foreach (var (gridUid, gridComp) in grids)
+        {
+            var tiles = _map.GetTilesIntersecting(gridUid, gridComp, radius);
+
+            var (gridPos, gridRot) = _transform.GetWorldPositionRotation(gridUid);
+
+            foreach (var tile in tiles)
+            {
+                var tileLocalPos = _map.ToCenterCoordinates(tile, gridComp);
+                var tilePos = gridPos + gridRot.RotateVec(tileLocalPos.Position);
+
+                if (!EntitiesBlocking(xrayPos, xrayMapId, tilePos))
+                    continue;
+
+                _overlay.Tiles[ent].Add(tile);
+            }
+        }
+
         ent.Comp.NextRefresh += ent.Comp.RefreshTime;
+    }
+
+    private bool EntitiesBlocking(Vector2 xrayPos, MapId xrayMapId, Vector2 targetPos)
+    {
+        var delta = targetPos - xrayPos;
+        var distance = delta.Length();
+
+        var ray = new CollisionRay(xrayPos, delta.Normalized(), (int)CollisionGroup.SingularityLayer);
+        return _physics.IntersectRayWithPredicate(xrayMapId, ray, distance, e => !HasComp<OccluderComponent>(e)).Any();
     }
 }
--- Content.Client/XRay/XRayOverlay.cs
+++ Content.Client/XRay/XRayOverlay.cs
@@ -1,7 +1,10 @@
 using Content.Shared.XRay;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
+using Robust.Client.ResourceManagement;
 using Robust.Shared.Enums;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
 using Robust.Shared.Prototypes;
 using System.Linq;
 using System.Numerics;
@@ -11,31 +14,75 @@ namespace Content.Client.XRay;
 public sealed class XRayOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _entityManager = default!;
+    [Dependency] private readonly ITileDefinitionManager _tileDef = default!;
     [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly IResourceCache _resource = default!;
 
+    private readonly EntityLookupSystem _lookups;
     private readonly SpriteSystem _sprite;
     private readonly SharedTransformSystem _transform;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
     public override bool RequestScreenTexture => true;
 
+    public Dictionary<Entity<ShowXRayComponent>, HashSet<TileRef>> Tiles = new();
     public Dictionary<Entity<ShowXRayComponent>, HashSet<EntityUid>> Entities = new();
 
     public XRayOverlay()
     {
         IoCManager.InjectDependencies(this);
+        _lookups = _entityManager.System<EntityLookupSystem>();
         _sprite = _entityManager.System<SpriteSystem>();
         _transform = _entityManager.System<SharedTransformSystem>();
     }
 
     protected override bool BeforeDraw(in OverlayDrawArgs args)
     {
-        return Entities.Any();
+        return Tiles.Any() || Entities.Any();
     }
 
     protected override void Draw(in OverlayDrawArgs args)
     {
         var eyeRot = args.Viewport.Eye?.Rotation ?? Angle.Zero;
+
+        Dictionary<EntityUid, MapGridComponent> mapGrids = new();
+        Dictionary<EntityUid, Matrix3x2> mapMatrix = new();
+
+        foreach (var (xray, tiles) in Tiles)
+        {
+            var shader = _prototype.Index<ShaderPrototype>(xray.Comp.Shader).InstanceUnique();
+            args.WorldHandle.UseShader(shader);
+
+            foreach (var tileRef in tiles)
+            {
+                if (!mapGrids.TryGetValue(tileRef.GridUid, out var mapGrid))
+                {
+                    if (!_entityManager.TryGetComponent(tileRef.GridUid, out mapGrid))
+                        continue;
+
+                    mapGrids.Add(tileRef.GridUid, mapGrid);
+                }
+
+                var tile = _tileDef[tileRef.Tile.TypeId];
+
+                if (tile.Sprite is not {} sprite)
+                    continue;
+
+                if (!mapMatrix.TryGetValue(tileRef.GridUid, out var transform))
+                {
+                    transform = _transform.GetWorldMatrix(tileRef.GridUid);
+                    mapMatrix.Add(tileRef.GridUid, transform);
+                }
+
+                args.DrawingHandle.SetTransform(transform);
+
+                var bounds = _lookups.GetLocalBounds(tileRef, mapGrid.TileSize);
+                var texture = _resource.GetResource<TextureResource>(sprite);
+
+                // TODO: maybe get decals too?
+                args.WorldHandle.DrawTextureRect(texture, bounds);
+            }
+        }
 
         foreach (var (xray, entities) in Entities)
         {
@@ -44,8 +91,7 @@ public sealed class XRayOverlay : Overlay
 
             foreach (var entity in entities)
             {
-                if (!_entityManager.TryGetComponent<SpriteComponent>(entity, out var sprite))
-                    continue;
+                var sprite = _entityManager.GetComponent<SpriteComponent>(entity);
 
                 var entityXform = _entityManager.GetComponent<TransformComponent>(entity);
                 var (entPos, entRot) = _transform.GetWorldPositionRotation(entityXform);
