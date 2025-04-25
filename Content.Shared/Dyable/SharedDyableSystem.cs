f33b942ea553fb73127aaa71182d837d8a18316b
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-04-25 16:07:18 +0100

    show colors for inhands

--- Content.Shared/Dyable/SharedDyableSystem.cs
+++ Content.Shared/Dyable/SharedDyableSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.Clothing.Components;
 using Content.Shared.Clothing.EntitySystems;
+using Content.Shared.Item;
 using JetBrains.Annotations;
 using Robust.Shared.Reflection;
 
@@ -9,6 +10,7 @@ public abstract partial class SharedDyableSystem : EntitySystem
 {
     [Dependency] protected readonly SharedAppearanceSystem Appearance = default!;
     [Dependency] private readonly ClothingSystem _clothing = default!;
+    [Dependency] private readonly SharedItemSystem _item = default!;
     [Dependency] private readonly IReflectionManager _reflection = default!;
 
     public override void Initialize()
@@ -22,17 +24,29 @@ public abstract partial class SharedDyableSystem : EntitySystem
     {
         Appearance.SetData(ent.Owner, DyableVisuals.Color, ent.Comp.Color);
         UpdateClothing(ent);
+        UpdateItem(ent);
     }
 
     private void UpdateClothing(Entity<DyableComponent> ent)
     {
+        if (!TryComp<ClothingComponent>(ent.Owner, out var clothing))
+            return;
+
         var key = _reflection.GetEnumReference(DyableVisualsLayers.Layer);
 
-        if (TryComp<ClothingComponent>(ent.Owner, out var clothing))
-        {
-            foreach (var slot in clothing.ClothingVisuals.Keys)
-                _clothing.SetLayerColor(clothing, slot, key, ent.Comp.Color);
-        }
+        foreach (var slot in clothing.ClothingVisuals.Keys)
+            _clothing.SetLayerColor(clothing, slot, key, ent.Comp.Color);
+    }
+
+    private void UpdateItem(Entity<DyableComponent> ent)
+    {
+        if (!TryComp<ItemComponent>(ent.Owner, out var item))
+            return;
+
+        var key = _reflection.GetEnumReference(DyableVisualsLayers.Layer);
+
+        foreach (var hand in item.InhandVisuals.Keys)
+            _item.SetLayerColor(ent.Owner, hand, key, ent.Comp.Color, item);
     }
 
     [PublicAPI]
@@ -45,5 +59,6 @@ public abstract partial class SharedDyableSystem : EntitySystem
         DirtyField(ent.Owner, ent.Comp, nameof(DyableComponent.Color));
         Appearance.SetData(ent.Owner, DyableVisuals.Color, ent.Comp.Color);
         UpdateClothing((ent.Owner, ent.Comp));
+        UpdateItem((ent.Owner, ent.Comp));
     }
 }
