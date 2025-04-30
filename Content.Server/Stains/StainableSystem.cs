58ec8a13515e326bc934c7e5bf3bb555ba5dac0b
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-04-30 15:35:51 +0100

    stainable forensics

--- Content.Server/Stains/StainableSystem.cs
+++ Content.Server/Stains/StainableSystem.cs
@@ -1,3 +1,4 @@
+using Content.Server.Forensics;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Stains;
 
@@ -5,6 +6,8 @@ namespace Content.Server.Stains;
 
 public sealed partial class StainableSystem : SharedStainableSystem
 {
+    [Dependency] private readonly ForensicsSystem _forensics = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -12,5 +15,9 @@ public sealed partial class StainableSystem : SharedStainableSystem
 
     protected override void WashingForensics(Entity<StainableComponent> ent, Entity<SolutionComponent> solution, EntityUid washingMachine)
     {
+        if (!TryComp<ForensicsComponent>(washingMachine, out var forensics))
+            return;
+
+        forensics.DNAs.UnionWith(_forensics.GetSolutionsDNA(solution.Comp.Solution));
     }
 }
