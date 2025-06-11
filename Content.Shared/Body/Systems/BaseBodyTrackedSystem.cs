bc17e500dc372dafc48c96ce695779a329f717fa
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-06-11 16:51:00 +0100

    allow multiple subscriptions

--- Content.Shared/Body/Systems/BaseBodyTrackedSystem.cs
+++ Content.Shared/Body/Systems/BaseBodyTrackedSystem.cs
@@ -4,37 +4,84 @@ namespace Content.Shared.Body.Systems;
 
 public abstract partial class BaseBodyTrackedSystem : EntitySystem
 {
+    private readonly Dictionary<string, Dictionary<string, Delegate>> _addSubscribers = [];
+    private readonly Dictionary<string, Dictionary<string, Delegate>> _removeSubscribers = [];
+
     protected void SubscribeTrackerAdded<TTracker, TTracked>(EntityEventRefHandler<TTracker, BodyTrackerAdded> handler)
-        where TTracker : Component
+        where TTracker : Component, new()
         where TTracked : Component, new()
     {
-        SubscribeLocalEvent<TTracker, BodyTrackerAdded>((u, c, a) => OnTrackerAdded<TTracker, TTracked>(u, c, ref a, handler));
+        var trackerName = Factory.GetComponentName<TTracker>();
+        var trackedName = Factory.GetComponentName<TTracked>();
+
+        if (_addSubscribers.TryGetValue(trackerName, out var trackers))
+        {
+            trackers.Add(trackedName, handler);
+        }
+        else
+        {
+            trackers = [];
+            trackers.Add(trackedName, handler);
+            _addSubscribers.Add(trackerName, trackers);
+            SubscribeLocalEvent<TTracker, BodyTrackerAdded>(OnTrackerAdded);
+        }
     }
 
-    private void OnTrackerAdded<TTracker, TTracked>(EntityUid uid, TTracker component, ref BodyTrackerAdded args, EntityEventRefHandler<TTracker, BodyTrackerAdded> handler)
-        where TTracker : Component
-        where TTracked : Component, new()
+    private void OnTrackerAdded<TTracker>(EntityUid uid, TTracker component, ref BodyTrackerAdded args) where TTracker : Component, new()
     {
-        if (args.ComponentName != Factory.GetComponentName<TTracked>())
+        var trackerName = Factory.GetComponentName<TTracker>();
+        if (!_addSubscribers.TryGetValue(trackerName, out var trackers))
             return;
 
-        handler.Invoke((uid, component), ref args);
+        foreach (var (tracked, handler) in trackers)
+        {
+            if (tracked != args.ComponentName)
+                continue;
+
+            if (handler is EntityEventRefHandler<TTracker, BodyTrackerAdded> castedHandler)
+            {
+                castedHandler.Invoke((uid, component), ref args);
+                return;
+            }
+        }
     }
 
     protected void SubscribeTrackerRemoved<TTracker, TTracked>(EntityEventRefHandler<TTracker, BodyTrackerRemoved> handler)
-        where TTracker : Component
+        where TTracker : Component, new()
         where TTracked : Component, new()
     {
-        SubscribeLocalEvent<TTracker, BodyTrackerRemoved>((u, c, a) => OnTrackerRemoved<TTracker, TTracked>(u, c, ref a, handler));
+        var trackerName = Factory.GetComponentName<TTracker>();
+        var trackedName = Factory.GetComponentName<TTracked>();
+
+        if (_removeSubscribers.TryGetValue(trackerName, out var trackers))
+        {
+            trackers.Add(trackedName, handler);
+        }
+        else
+        {
+            trackers = [];
+            trackers.Add(trackedName, handler);
+            _removeSubscribers.Add(trackerName, trackers);
+            SubscribeLocalEvent<TTracker, BodyTrackerRemoved>(OnTrackerRemoved);
+        }
     }
 
-    private void OnTrackerRemoved<TTracker, TTracked>(EntityUid uid, TTracker component, ref BodyTrackerRemoved args, EntityEventRefHandler<TTracker, BodyTrackerRemoved> handler)
-        where TTracker : Component
-        where TTracked : Component, new()
+    private void OnTrackerRemoved<TTracker>(EntityUid uid, TTracker component, ref BodyTrackerRemoved args) where TTracker : Component, new()
     {
-        if (args.ComponentName != Factory.GetComponentName<TTracked>())
+        var trackerName = Factory.GetComponentName<TTracker>();
+        if (!_removeSubscribers.TryGetValue(trackerName, out var trackers))
             return;
 
-        handler.Invoke((uid, component), ref args);
+        foreach (var (tracked, handler) in trackers)
+        {
+            if (tracked != args.ComponentName)
+                continue;
+
+            if (handler is EntityEventRefHandler<TTracker, BodyTrackerRemoved> castedHandler)
+            {
+                castedHandler.Invoke((uid, component), ref args);
+                return;
+            }
+        }
     }
 }
