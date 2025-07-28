fa604cb991e7ce5271b5c66c7410bad8933f7b63
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-07-28 10:00:19 +0100

    feat: dont deal pain when patient is dead

---
+++ Content.Shared/Surgery/Pain/Requirements/MobState.cs
@@ -0,0 +1,25 @@
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
+using JetBrains.Annotations;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Surgery.Pain.Requirements;
+
+[UsedImplicitly]
+[Serializable, NetSerializable]
+public sealed partial class RequireMobState : SurgeryPainRequirement
+{
+    [DataField]
+    public HashSet<MobState> AllowedStates = [];
+
+    public override bool RequirementMet(IEntityManager entity, EntityUid? body, EntityUid? limb, EntityUid? used)
+    {
+        if (body == null)
+            return false;
+
+        if (!entity.TryGetComponent<MobStateComponent>(body.Value, out var mobState))
+            return true;
+
+        return AllowedStates.Contains(mobState.CurrentState);
+    }
+}
