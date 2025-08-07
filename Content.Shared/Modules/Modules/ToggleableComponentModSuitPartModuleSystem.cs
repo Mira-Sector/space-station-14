d226b3be1ea79d5d5a720378c8653cac24f26b1c
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-08-07 01:47:08 +0100

    fix mispredict on component modules

--- Content.Shared/Modules/Modules/ToggleableComponentModSuitPartModuleSystem.cs
+++ Content.Shared/Modules/Modules/ToggleableComponentModSuitPartModuleSystem.cs
@@ -1,13 +1,11 @@
 using Content.Shared.Modules.Components.Modules;
 using Content.Shared.Modules.Events;
 using Content.Shared.Modules.ModSuit;
-using Robust.Shared.Timing;
 
 namespace Content.Shared.Modules.Modules;
 
 public sealed partial class ToggleableComponentModSuitPartModuleSystem : EntitySystem
 {
-    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly SharedModSuitSystem _modSuit = default!;
 
     public override void Initialize()
@@ -20,20 +18,14 @@ public sealed partial class ToggleableComponentModSuitPartModuleSystem : EntityS
 
     private void OnEnabled(Entity<ToggleableComponentModSuitPartModuleComponent> ent, ref ModuleEnabledEvent args)
     {
-        if (!_timing.IsFirstTimePredicted)
-            return;
-
         if (!_modSuit.TryGetDeployedPart(args.Container, ent.Comp.PartType, out var foundPart))
             return;
 
-        EntityManager.AddComponents(foundPart.Value, ent.Comp.Components);
+        EntityManager.AddComponents(foundPart.Value, ent.Comp.Components, true);
     }
 
     private void OnDisabled(Entity<ToggleableComponentModSuitPartModuleComponent> ent, ref ModuleDisabledEvent args)
     {
-        if (!_timing.IsFirstTimePredicted)
-            return;
-
         if (!_modSuit.TryGetDeployedPart(args.Container, ent.Comp.PartType, out var foundPart))
             return;
 
