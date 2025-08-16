e812edf821ee9322c780972e97ae108cca68189d
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-08-16 21:27:39 +0100

    solar flare visuals

--- Content.Client/StationEvents/SolarFlareOverlay.cs
+++ Content.Client/StationEvents/SolarFlareOverlay.cs
@@ -1,33 +1,51 @@
+using Content.Client.Parallax;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
+using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
 
 namespace Content.Client.StationEvents;
 
 public sealed class SolarFlareOverlay : Overlay
 {
-    public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowFOV;
-    public override bool RequestScreenTexture => true;
+    public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowWorld;
+
+    private const float OffsetMinMagnitude = 1.414213562f;
+    private const float OffsetMaxMagnitude = 1.5f;
 
     [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
 
     private readonly ShaderInstance _shader;
+
 
     public SolarFlareOverlay() : base()
     {
         IoCManager.InjectDependencies(this);
+
+        ZIndex = ParallaxSystem.ParallaxZIndex + 1;
+
         _shader = _prototype.Index<ShaderPrototype>("SolarFlare").InstanceUnique();
+        _shader.SetParameter("seed", _random.NextFloat());
+        _shader.SetParameter("offset", _random.NextVector2(OffsetMinMagnitude, OffsetMaxMagnitude));
+    }
+
+    protected override bool BeforeDraw(in OverlayDrawArgs args)
+    {
+        if (args.MapId == MapId.Nullspace)
+            return false;
+
+        return true;
     }
 
     protected override void Draw(in OverlayDrawArgs args)
     {
-        if (ScreenTexture == null)
+        if (args.MapId == MapId.Nullspace)
             return;
 
-        var handle = args.WorldHandle;
-        _shader.SetParameter("SCREEN_TEXTURE", ScreenTexture);
-        handle.UseShader(_shader);
-        handle.DrawRect(args.WorldBounds, Color.White);
-        handle.UseShader(null);
+        args.WorldHandle.UseShader(_shader);
+        args.WorldHandle.DrawRect(args.WorldBounds, Color.White);
+        args.WorldHandle.UseShader(null);
     }
 }
