a257d94f1078bfdd18b51be9b1abd8b894783d35
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2025-10-25 05:32:33 +0100

    only send relevant chunks to clients

--- Content.Server/Shadows/ShadowSystem.cs
+++ Content.Server/Shadows/ShadowSystem.cs
@@ -1,6 +1,8 @@
+using Content.Shared.Chunking;
 using Content.Shared.Shadows;
 using Content.Shared.Shadows.Components;
 using Content.Shared.Shadows.Events;
+using Microsoft.Extensions.ObjectPool;
 using Robust.Shared.GameStates;
 using Robust.Shared.Player;
 
@@ -8,6 +10,16 @@ namespace Content.Server.Shadows;
 
 public sealed partial class ShadowSystem : SharedShadowSystem
 {
+    [Dependency] private readonly ChunkingSystem _chunking = default!;
+
+    private readonly ObjectPool<HashSet<Vector2i>> _chunkIndexPool =
+        new DefaultObjectPool<HashSet<Vector2i>>(
+            new DefaultPooledObjectPolicy<HashSet<Vector2i>>(), 64);
+
+    private readonly ObjectPool<Dictionary<NetEntity, HashSet<Vector2i>>> _chunkViewerPool =
+        new DefaultObjectPool<Dictionary<NetEntity, HashSet<Vector2i>>>(
+            new DefaultPooledObjectPolicy<Dictionary<NetEntity, HashSet<Vector2i>>>(), 64);
+
     public override void Initialize()
     {
         base.Initialize();
@@ -17,7 +29,39 @@ public sealed partial class ShadowSystem : SharedShadowSystem
 
     private void OnGridGetState(Entity<ShadowGridComponent> ent, ref ComponentGetState args)
     {
-        args.State = new ShadowGridState(GetNetEntitySet(ent.Comp.Casters), ent.Comp.Chunks);
+        if (args.Player is not { } session)
+        {
+            // send full state
+            args.State = new ShadowGridState(GetNetEntitySet(ent.Comp.Casters), ent.Comp.Chunks);
+            return;
+        }
+
+        var netGrid = GetNetEntity(ent.Owner);
+
+        var chunksInRange = _chunking.GetChunksForSession(session, ShadowGridComponent.ChunkSize, _chunkIndexPool, _chunkViewerPool);
+        if (!chunksInRange.TryGetValue(netGrid, out var chunkIndexes))
+            return;
+
+        try
+        {
+            Dictionary<Vector2i, ShadowChunk> toSend = new(chunkIndexes.Count);
+            foreach (var chunkIndex in chunkIndexes)
+            {
+                if (!ent.Comp.Chunks.TryGetValue(chunkIndex, out var chunk))
+                    continue;
+
+                toSend[chunkIndex] = chunk;
+            }
+
+            args.State = new ShadowGridState(GetNetEntitySet(ent.Comp.Casters), toSend);
+        }
+        finally
+        {
+            foreach (var chunk in chunksInRange.Values)
+                _chunkIndexPool.Return(chunk);
+
+            _chunkViewerPool.Return(chunksInRange);
+        }
     }
 
 #if DEBUG
